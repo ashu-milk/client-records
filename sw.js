777c29df89dd906b36679aeb8f5061742980ac64
ashu-milk <ashumilku02@gmail.com>
2026-08-27 21:02:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'client-records-v11';
+const CACHE_NAME = 'client-records-v12';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-48.png', './icon-72.png', './icon-96.png', './icon-144.png', './icon-192.png', './icon-512.png'];
 
 // インストール時: アプリ本体を確実にキャッシュ
